ef5875b01edd7a3b67259e0c6ebcc8309ffe5e13
tamilselvan083 <144792489+tamilselvan083@users.noreply.github.com>
2023-09-13 12:58:08 +0530

    Create Unit 2 challenge 2.py

---
+++ Unit 2 challenge 2.py
@@ -0,0 +1,15 @@
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
