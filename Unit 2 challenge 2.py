c69c57daff50cf893445fd0857ff520b0caceede
tamilselvan083 <144792489+tamilselvan083@users.noreply.github.com>
2023-09-19 14:47:30 +0530

    Update Unit 2 challenge 2.py

--- Unit 2 challenge 2.py
+++ Unit 2 challenge 2.py
@@ -1,8 +1,8 @@
-class Batsman(Player):
+class Batsman():
     def play(self):
         print("The batsman is batting.")
 
-class Bowler(Player):
+class Bowler():
     def play(self):
         print("The bowler is bowling.")
 
